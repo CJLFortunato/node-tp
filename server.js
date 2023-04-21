e700e3598c892fc231d2f50233ab71029069992c
CJL Fortunato <clemence.fortunato@gmail.com>
2023-04-21 14:03:55 +0200

    Add delete functionality

--- server.js
+++ server.js
@@ -13,58 +13,80 @@ import {
 
 dotenv.config()
 
-const users = USERS.map(user => ({
+let users = USERS.map(user => ({
     ...user,
     birth: dayjs(user.birth).format('DD/MM/YYYY')
 }))
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    switch (req.url) {
-        case '/Assets/CSS/style.css':
-            const cssFile = fs.readFileSync('Assets/CSS/style.css', 'utf8');
-            res.writeHead(200, {
-                'Content-Type': 'text/css'
+
+    if (req.url === '/Assets/CSS/style.css') {
+        const cssFile = fs.readFileSync('Assets/CSS/style.css', 'utf8');
+        res.writeHead(200, {
+            'Content-Type': 'text/css'
+        });
+        res.write(cssFile);
+        res.end();
+    } else if (req.url === '/') {
+        const homeTemplate = fs.readFileSync('View/home.ejs', 'utf8');
+        const homeRendered = ejs.render(homeTemplate);
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(homeRendered);
+        res.end();
+    } else if (req.url.includes('/users')) {
+        let body = '';
+        const name = req.url.split('/')[2];
+        console.log('test');
+        console.log(name);
+        console.log(req.method);
+        req.on('data', chunk => {
+            body += chunk.toString();
+            // console.log('test');
+            // console.log(body)
+        });
+        req.on('end', () => {
+            const formData = parse(body);
+            console.log(formData);
+            if (formData.name) users.push({
+                ...formData,
+                birth: dayjs(formData.birth).format('DD/MM/YYYY')
             });
-            res.write(cssFile);
-            res.end();
-        case '/':
-            const homeTemplate = fs.readFileSync('View/home.ejs', 'utf8');
-            const homeRendered = ejs.render(homeTemplate);
+            const usersTemplate = fs.readFileSync('View/users.ejs', 'utf8');
+            const usersRendered = ejs.render(usersTemplate, {
+                users
+            });
             res.writeHead(200, {
                 'Content-Type': 'text/html'
             });
-            res.write(homeRendered);
+            res.write(usersRendered);
             res.end();
-            break;
-        case '/users':
-            let body = '';
-            req.on('data', chunk => {
-                body += chunk.toString();
-                // console.log('test');
-                // console.log(body)
+        });
+    } else if (req.url.includes('/delete/')) {
+        console.log('test');
+        console.log(req.url);
+        const name = req.url.split('/')[2];
+        console.log(name);
+        req.on('data', chunk => {
+            body += chunk.toString();
+            console.log(body)
+        });
+        req.on('end', () => {
+            users = users.filter(user => user.name !== name)
+            const deletedTemplate = fs.readFileSync('View/deleted.ejs', 'utf8');
+            const deletedRendered = ejs.render(deletedTemplate, {
+                name
             });
-            req.on('end', () => {
-                const formData = parse(body);
-                console.log(formData);
-                if (formData.name) users.push({
-                    ...formData,
-                    birth: dayjs(formData.birth).format('DD/MM/YYYY')
-                });
-                const usersTemplate = fs.readFileSync('View/users.ejs', 'utf8');
-                const usersRendered = ejs.render(usersTemplate, {
-                    users
-                });
-                res.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                res.write(usersRendered);
-                res.end();
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
             });
-            break;
-        case '/user/':
-            console.log(req.url);
+            res.write(deletedRendered);
+            res.end();
+        });
     }
+    return;
 });
 
 server.listen(process.env.APP_PORT, () => {
